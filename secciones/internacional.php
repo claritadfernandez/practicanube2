a8d8a1619c1093c1c2d02e0f09491c4bdbabcef4
claritadfernandez <142109726+claritadfernandez@users.noreply.github.com>
2024-01-09 22:06:35 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Violencia en Gaza",
+"autor" => "Pablo Antonio",
+"resumen" => "Edificios completamente destruidos en Gaza",
 ];
 ?>
